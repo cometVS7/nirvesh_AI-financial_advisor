72b30fc324514dfe7304e08c2676e1b26e8e7e25
VARDHAN SHAH <mailvardhanshah@gmail.com>
2026-04-18 10:10:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 from backend.api import app
 
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://v0-nirvesh-ai-dashboard.vercel.app", "http://localhost:3000","https://nirveshai-financialadvisor.streamlit.app/"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
